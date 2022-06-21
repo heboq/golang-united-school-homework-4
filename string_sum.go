d0c0c99f3f9ba5c7a571228940bdb7fda0f3f002
Mark <61473070+heboq@users.noreply.github.com>
2022-06-22 03:32:33 +0700

    add solution

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -16,12 +19,33 @@ var (
 // For example, having an input string "3+5", it should return output string "8" and nil error
 // Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
 //
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
+// For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
 // the function should return an empty string and an appropriate error from strconv package wrapped into your own error
 // with fmt.Errorf function
 //
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	replaced := strings.NewReplacer(" ", "", "+", ".+", "-", ".-").Replace(input)
+	replaced = strings.TrimPrefix(replaced, ".")
+
+	if len(replaced) == 0 {
+		return "", fmt.Errorf("string_sum.StringSum: summing \"%s\": %w", input, errorEmptyInput)
+	}
+
+	numbers := strings.Split(replaced, ".")
+
+	if len(numbers) != 2 {
+		return "", fmt.Errorf("string_sum.StringSum: summing \"%s\": %w", input, errorNotTwoOperands)
+	}
+
+	var sum int
+	for _, s := range numbers {
+		num, err := strconv.Atoi(s)
+		if err != nil {
+			return "", fmt.Errorf("string_sum.StringSum: summing \"%s\": %w", input, err)
+		}
+		sum += num
+	}
+	return strconv.Itoa(sum), nil
 }
